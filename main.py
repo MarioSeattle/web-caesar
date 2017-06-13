76cbcf5145962d0cda63bbc130e310345f42a8f8
MarioSeattle <marioseattle@live.com>
2017-06-12 18:01:01 -0700

    Finished Caesar lol

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -32,7 +32,7 @@ form = """
                 <input type="text" name="rot" value="0">
                 <p class="error"></p>
             </div>
-            <textarea type="text" name="text">o</textarea>
+            <textarea type="text" name="text">{0}</textarea>
             <br>
             <input type="submit">
         </form>
@@ -43,15 +43,15 @@ form = """
 @app.route("/")
 def index():
 
-    return form
+    return form.format("")
 
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
-    text = request.form['text']
+    text = request.form['text'] 
     encrypt = rotate_string(text, rot)
 
-    return '<h1>' + encrypt + '</h1>'
+    return form.format(encrypt) 
 
 app.run()
